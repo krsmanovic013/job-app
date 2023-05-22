d7de9629ffb6f6989a2722ba818d077166d1c21c
unknown <krsmanovic013@gmail.com>
2023-05-22 02:34:24 +0200

    login and register done, local storage and custom fetch with axios

--- src/App.jsx
+++ src/App.jsx
@@ -7,10 +7,10 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="\" element={<Dashboard />} />
+        <Route path="/" element={<Dashboard />} />
         <Route path="landing" element={<Landing />} />
         <Route path="register" element={<Register />} />
-        <Route path="*" element={<Error />} />
+        <Route path="error" element={<Error />} />
       </Routes>
       <ToastContainer position="top-center" />
     </BrowserRouter>
--- src/Pages/Register.jsx
+++ src/Pages/Register.jsx
@@ -2,6 +2,9 @@ import { useState, useEffect } from "react";
 import { Logo, FormRow } from "../components";
 import Wrapper from "../assets/wrappers/RegisterPage";
 import { toast } from "react-toastify";
+import { useDispatch, useSelector } from "react-redux";
+import { loginUser, registerUser } from "../features/user/userSlice";
+import { useNavigate } from "react-router-dom";
 
 const initialState = {
   name: "",
@@ -14,6 +17,9 @@ const initialState = {
 
 const Register = () => {
   const [values, setValues] = useState(initialState);
+  const { isLoading, user } = useSelector((store) => store.user);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   const handleChange = (e) => {
     setValues({ ...values, [e.target.name]: e.target.value });
@@ -25,13 +31,27 @@ const Register = () => {
 
     if ((!isMember && !name) || !email || !password) {
       toast.error("Please Fill Out All Fileds");
+      return;
     }
-    setValues(initialState);
+    if (isMember) {
+      dispatch(loginUser({ email: email, password: password }));
+      return;
+    }
+    dispatch(registerUser({ name, email, password }));
+    // setValues(initialState);
   };
 
   const toggleMember = () => {
     setValues({ ...values, isMember: !values.isMember });
   };
+
+  useEffect(() => {
+    if (user) {
+      setTimeout(() => {
+        navigate("/");
+      }, 2000);
+    }
+  }, [user]);
 
   return (
     <Wrapper className="full-page">
@@ -59,8 +79,8 @@ const Register = () => {
           value={values.password}
           handleChange={handleChange}
         />
-        <button type="submit" className="btn btn-block">
-          submit
+        <button type="submit" className="btn btn-block" disabled={isLoading}>
+          {isLoading ? "loading..." : "submit"}
         </button>
         <p>
           {values.isMember ? "Not a member yet?" : "Already a member?"}
